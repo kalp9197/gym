41d638211cc80bb90deb3ac578e0414e5df3e813
kalp9197 <pkalp61@example.com>
2023-02-25 19:31:47 +0530

    Added Validations

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-function success(){
-  Swal.fire(
-    'Good job!',
-    'Successfully Submitted',
-    'success'
-  ) 
+// function success(){
+//   Swal.fire(
+//     'Good job!',
+//     'Successfully Submitted',
+//     'success'
+//   ) 
+// }
+
+
+function validate(){
+  user_name=document.getElementById("name").value
+  user_email=document.getElementById("email").value
+  user_membership=document.getElementById("membership").value
+
+  if (user_name!="" && user_email!="" &&user_membership!="" )
+  {
+    Swal.fire(
+          'Good job!',
+          'Successfully Submitted',
+          'success'
+         ) 
+  }
+  else{
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Something went wrong!',
+    })
+  }
+
 }
