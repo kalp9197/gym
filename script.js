749c68f791ebd47b5aee7ac62c61567ed9ea8946
kalp9197 <pkalp61@example.com>
2023-02-25 22:38:23 +0530

    Added Font

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-// function success(){
-//   Swal.fire(
-//     'Good job!',
-//     'Successfully Submitted',
-//     'success'
-//   ) 
-// }
-
-
 function validate(){
-  user_name=document.getElementById("name").value
-  user_email=document.getElementById("email").value
-  user_membership=document.getElementById("membership").value
+ let user_name=document.getElementById("name").value
+ let user_email=document.getElementById("email").value
+ let user_membership=document.getElementById("membership").value
 
-  if (user_name!="" && user_email!="" &&user_membership!="" )
-  {
-    Swal.fire(
-          'Good job!',
-          'Successfully Submitted',
-          'success'
-         ) 
-  }
-  else{
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: 'Something went wrong!',
-    })
-  }
-
+Swal.fire(
+       'Good job!',
+       'Your Response Has Successfully Submitted',
+       'success'
+     ) 
 }
